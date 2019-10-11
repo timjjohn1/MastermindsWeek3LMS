01d845fe18bdd30915386c912119016db4e1a421
Tim Johnson <tim.johnson@smoothstack.com>
2019-10-11 14:23:45 -0400

    test push

--- src/main/java/com/ss/lms/secret/ServerCredentials.java
+++ src/main/java/com/ss/lms/secret/ServerCredentials.java
@@ -2,7 +2,7 @@ package com.ss.lms.secret;
 
 public final class ServerCredentials 
 {
-	public final static String username = "myUsername";
-	public final static String password = "myPass";
+	public final static String username = "root";
+	public final static String password = "";
 	public final static String serverUrl = "jdbc:mysql://localhost:3306/library?useSSL=false";
 }
