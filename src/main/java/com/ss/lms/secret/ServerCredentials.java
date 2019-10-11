f5f35cb4b4e1529b3f906c8ca3fa039f484b676c
NicolaeVSS <nicolae.vasile@smoothstack.com>
2019-10-11 14:49:04 -0400

    removed server credentials, not possible to achieve goal

--- src/main/java/com/ss/lms/secret/ServerCredentials.java
+++
@@ -1,8 +0,0 @@
-package com.ss.lms.secret;
-
-public final class ServerCredentials 
-{
-	public final static String username = "root";
-	public final static String password = "Sgdfgef";
-	public final static String serverUrl = "jdbc:mysql://localhost:3306/library?useSSL=false";
-}
