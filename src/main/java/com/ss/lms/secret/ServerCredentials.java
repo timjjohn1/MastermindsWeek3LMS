32b47f51c3b9683344210d4171c2d071ba31056a
nicolae <nicolae@nicolaeLaptop>
2019-10-11 14:13:50 -0400

    created ServerCredentials, added to .gitignore. removing from index next

---
+++ src/main/java/com/ss/lms/secret/ServerCredentials.java
@@ -0,0 +1,8 @@
+package com.ss.lms.secret;
+
+public final class ServerCredentials 
+{
+	public final static String username = "root";
+	public final static String password = "";
+	public final static String serverUrl = "jdbc:mysql://localhost:3306/library?useSSL=false";
+}
